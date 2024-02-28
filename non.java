c42e35b7a25568ced0d3461d8a8b660f2635b58e
ayushmaan-gupta <155837090+ayushmaan-gupta@users.noreply.github.com>
2024-02-28 13:07:30 +0000

    java

---
+++ non.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class hello {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        int a, b;
+        System.out.println("enter 2 num");
+        a = obj.nextInt();
+        b = obj.nextInt();
+        System.out.println(a + b);
+    }
+}
